3d3758803b2b82045efc1fd0f545d516ed0a5033
kjain1810 <kunaljain.2000@yahoo.in>
2022-03-12 19:18:00 +0530

    Making Ui

--- 7.cpp
+++ 7.cpp
@@ -8,12 +8,12 @@ class Game {
     {
       this->players = 0;
       Si = vector<int>();
-      Ui = map<vector<int>, vector<int>> ();
+      U = map<vector<int>, vector<int>> ();
     }
     int players;
     vector<int> Si;
-    map<vector<int>, vector<int>> Ui; 
-
+    map<vector<int>, vector<int>> U; 
+    vector<vector<map<vector<int>, int>>> Ui;
     void recursive_strategy(int player, vector<int> &strats)
     {
       if(player == -1)
@@ -24,7 +24,7 @@ class Game {
         vector<int> new_strats;
         for(int a = this->players - 1; a >= 0; a--)
           new_strats.push_back(strats[a]);
-        Ui[new_strats] = here;
+        U[new_strats] = here;
         return;
       }
       for(int a = 0; a < this->Si[player]; a++)
@@ -33,6 +33,64 @@ class Game {
         recursive_strategy(player - 1, strats);
         strats.pop_back();
       }
+    }
+
+    void _buildUi(int c, int &tp, int &ts, vector<int> &strats)
+    {
+      if(c == tp)
+      {
+        strats.push_back(ts);
+        this->_buildUi(c + 1, tp, ts, strats);
+        strats.pop_back();
+      }
+      else if(c == this->players)
+        this->Ui[tp][ts].insert({strats, this->U[strats][tp]});
+      else
+      {
+        for(int a = 0; a < this->Si[c]; a++)
+        {
+          strats.push_back(a);
+          this->_buildUi(c + 1, tp, ts, strats);
+          strats.pop_back();
+        }
+      }
+    }
+
+    void buildUi()
+    {
+      for(int a = 0; a < this->players; a++)
+      {
+        vector<map<vector<int>, int>> vec;
+        for(int b = 0; b < this->Si[a]; b++)
+        {
+          map<vector<int>, int> mep;
+          vec.push_back(mep);
+        }
+        this->Ui.push_back(vec);
+      }
+      for(int a = 0; a < this->players; a++)
+        for(int b = 0; b < this->Si[a]; b++)
+        {
+          vector<int> vec;
+          this->_buildUi(0, a, b, vec);
+        }
+    }
+    
+    void checkUi()
+    {
+      for(int a = 0; a < this->players; a++)
+        for(int b = 0; b < this->Si[a]; b++)
+        {
+          cout << "Player " << a << " Strategy " << b << " size is " << this->Ui[a][b].size() << endl;
+          for(auto u: this->Ui[a][b])
+          {
+            cout << "Strategy set: ";
+            for(auto s: u.first)
+              cout << s << " ";
+            cout << endl;
+            cout << "Utility: " << u.second << endl;
+          }
+        }
     }
 
     void input()
@@ -47,7 +105,7 @@ class Game {
 
     void checkInput()
     {
-      for(auto it = this->Ui.begin(); it != this->Ui.end(); it++)
+      for(auto it = this->U.begin(); it != this->U.end(); it++)
       {
         cout << "Strategies: ";
         for(auto f: it->first)
@@ -65,5 +123,7 @@ int main()
   Game game;
   game.input();
   game.checkInput();
+  game.buildUi();
+  game.checkUi();
   return 0;
 }
